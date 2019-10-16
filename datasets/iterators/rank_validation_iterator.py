29f0e51eed7e26bc96452942fe455a42a9a9eb24
Dhruvesh Patel <dhruvdplayer@gmail.com>
2019-10-16 18:48:20 -0400

    Added validation data pipeline for rank based validation

--- datasets/iterators/rank_validation_iterator.py
+++ datasets/iterators/rank_validation_iterator.py
@@ -1,4 +1,5 @@
 from allennlp.data.iterators import BasicIterator, DataIterator
+from typing import Tuple
 
 
 @DataIterator.register('single-sample-rank-validation-iterator')
@@ -7,6 +8,14 @@ class SingleSampleRankValidationIterator(BasicIterator):
     that the batch size is 1. Because the validation logic depends on
     it"""
 
-    def __init__(self, **kwargs):
-        batch_size = kwargs.pop('batch_size', 1)
-        super().__init__(batch_size=batch_size, **kwargs)
+    def __init__(
+            self,
+            batch_size: int = 1,
+            instances_per_epoch: int = None,
+            max_instances_in_memory: int = None,
+            cache_instances: bool = False,
+            track_epoch: bool = False,
+            maximum_samples_per_batch: Tuple[str, int] = None,
+    ):
+        batch_size = 1
+        super().__init__(batch_size=batch_size)

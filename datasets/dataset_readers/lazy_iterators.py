3298d76ad7a1b26c1eed6c3b731b042e3b361c14
Dhruvesh Patel <dhruvdplayer@gmail.com>
2019-11-12 23:30:43 -0500

    Added classification training dataset reader

--- datasets/dataset_readers/lazy_iterators.py
+++ datasets/dataset_readers/lazy_iterators.py
@@ -29,6 +29,28 @@ class LazyIteratorWithNegativeSampling(Iterable):
                                                negative_sample)
 
 
+class LazyIteratorWithSequentialNegativeSampling(
+        LazyIteratorWithNegativeSampling):
+    """Same as parent except that instead of generating (pos_sample, neg_sample),
+    it will generate two samples one after the other, one pos and negatives after that.
+
+    .. note:: The change in the signature of samples_to_instance Callable.
+    """
+
+    def __init__(self, negative_sampler: Callable[[Any], Any],
+                 positive_samples: Iterable[Any],
+                 samples_to_instance: Callable[[Any, int], Instance]):
+        super().__init__(negative_sampler, positive_samples,
+                         samples_to_instance)
+
+    def __iter__(self):
+        for positive_sample in self.positive_samples:
+            yield self.samples_to_instance(positive_sample, 1)
+
+            for negative_sample in self.negative_sampler(positive_sample):
+                yield self.samples_to_instance(negative_sample, 0)
+
+
 class LazyIteratorWithSingleNegativeSampling(LazyIteratorWithNegativeSampling):
     """ Every call to __iter__ will dynamically generate
     the list of samples.
